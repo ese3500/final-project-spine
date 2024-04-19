04582c5a8e0740b1aa87d882be0af3b65b3d14e5
Steven Walxim <56495540+walxim@users.noreply.github.com>
2024-04-18 21:52:06 -0400

    Updated angle added lowerback detection

--- Algo_main.c
+++ Algo_main.c
@@ -2,28 +2,39 @@
 #include <math.h>
 #include <time.h>
 
-#define NUM_SENSORS 4
-#define POSTURE_THRESHOLD 10.0 // Adjust this value based on your requirements
-#define NECK_ANGLE_THRESHOLD 20.0 // Adjust this value based on your requirements
-#define TIME_THRESHOLD 30 // Adjust this value based on your requirements (in seconds)
+#define NECK_ANGLE_THRESHOLD 15.0
+#define BACK_ANGLE_THRESHOLD 5.0
+#define TIME_THRESHOLD 30
 
-typedef struct {
-    double yaw;
-    double pitch;
-    double roll;
-} SensorData;
-
-void checkPosture(SensorData sensors[], time_t startTime) {
-    double neckAngle = sensors[0].pitch;
-    double upperBackAngle = sensors[1].pitch;
-    double middleBackAngle = sensors[2].pitch;
-    double lowerBackAngle = sensors[3].pitch;
+void checkPosture(double pitch1, double pitch2, double pitch3, time_t startTime) {
+    double neckAngle = pitch1;
+    double upperBackAngle = pitch2;
+    double lowerBackAngle = pitch3;
 
     // Check if the neck angle is too flat compared to the other sensors
     if (fabs(neckAngle - upperBackAngle) > NECK_ANGLE_THRESHOLD ||
-        fabs(neckAngle - middleBackAngle) > NECK_ANGLE_THRESHOLD ||
         fabs(neckAngle - lowerBackAngle) > NECK_ANGLE_THRESHOLD) {
         printf("Neck angle is too flat! Please adjust your posture.\n");
+    }
+
+    // Check if the neck is more than 20 degrees forward of the upper back
+    if (neckAngle - upperBackAngle > BACK_ANGLE_THRESHOLD) {
+        printf("Bad posture: Hunch back/head too much forward.\n");
+    }
+
+    // Check if the lower back is 20 degrees back of the upper back
+    if (upperBackAngle - lowerBackAngle > BACK_ANGLE_THRESHOLD) {
+        printf("Bad posture: Anterior pelvic tilt.\n");
+    }
+
+    // Check if the upper back angle is too hunched
+    if (upperBackAngle > 20.0) {
+        printf("Upper back is too hunched! Please sit up straight.\n");
+    }
+
+    // Check if the lower back angle is too arched
+    if (lowerBackAngle < -20.0) {
+        printf("Lower back is too arched! Please adjust your posture.\n");
     }
 
     // Check if the relative location has remained the same for too long
@@ -36,22 +47,38 @@ void checkPosture(SensorData sensors[], time_t startTime) {
 }
 
 int main() {
-    SensorData sensors[NUM_SENSORS];
+    double pitch1, yaw1, roll1;
+    double pitch2, yaw2, roll2;
+    double pitch3, yaw3, roll3;
     time_t startTime = time(NULL);
 
     // Input data from each sensor
-    for (int i = 0; i < NUM_SENSORS; i++) {
-        printf("Enter data for sensor %d:\n", i + 1);
-        printf("Yaw: ");
-        scanf("%lf", &sensors[i].yaw);
-        printf("Pitch: ");
-        scanf("%lf", &sensors[i].pitch);
-        printf("Roll: ");
-        scanf("%lf", &sensors[i].roll);
-    }
+    printf("Enter data for sensor 1 (neck):\n");
+    printf("Pitch: ");
+    scanf("%lf", &pitch1);
+    printf("Yaw: ");
+    scanf("%lf", &yaw1);
+    printf("Roll: ");
+    scanf("%lf", &roll1);
+
+    printf("Enter data for sensor 2 (upper back):\n");
+    printf("Pitch: ");
+    scanf("%lf", &pitch2);
+    printf("Yaw: ");
+    scanf("%lf", &yaw2);
+    printf("Roll: ");
+    scanf("%lf", &roll2);
+
+    printf("Enter data for sensor 3 (lower back):\n");
+    printf("Pitch: ");
+    scanf("%lf", &pitch3);
+    printf("Yaw: ");
+    scanf("%lf", &yaw3);
+    printf("Roll: ");
+    scanf("%lf", &roll3);
 
     // Check posture and timer
-    checkPosture(sensors, startTime);
+    checkPosture(pitch1, pitch2, pitch3, startTime);
 
     return 0;
 }
